bd9249178087fc07d981c0db03f2a0a666fc1395
Corey Roberts <spacepyro@gmail.com>
2020-10-10 09:54:47 -0500

    Restarting this project.

--- Example/Pods/Target Support Files/Pods-Sakura_Tests/Pods-Sakura_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sakura_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Sakura_Tests
-@end
--- Example/Pods/Target Support Files/Sakura/Sakura-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Sakura : NSObject
-@end
-@implementation PodsDummy_Sakura
-@end
